3ab9182785dc8951415bcc1158f7c6c6b3d9c0b2
Mary-7777 <maryna.mitroshyna@gmail.com>
2023-06-11 10:50:12 +0300

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
